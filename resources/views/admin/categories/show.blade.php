0e9a9adf75a05e8d5f57ef058f4f19e2d5563a59
RenatoConfuorto <renato.confuorto@outlook.it>
2022-07-12 07:40:16 +0200

    aggiunto collegamento ai post dalle categorie

--- resources/views/admin/categories/show.blade.php
+++ resources/views/admin/categories/show.blade.php
@@ -1,5 +1,17 @@
 @extends('dashboard')
 
 @section('content')
-  {{ dd($posts) }}
+
+    <div class="ms_card-container">
+
+        @foreach ($posts as $post)
+            <div class="card">
+                {{-- <img src="..." class="card-img-top" alt="..."> --}}
+                <div class="card-body">
+                    <h5 class="card-title">{{ $post->title }}</h5>
+                    <a href="{{ route('admin.posts.show', ['post' => $post->id]) }}" class="btn btn-primary">Vedi il post</a>
+                </div>
+            </div>
+        @endforeach
+    </div>
 @endsection
